8c7a7f9442c63954e658db59997c0fff711b8cb4
Nick Dumas <drakonik@gmail.com>
2018-05-24 01:38:45 +0000

    Update tests.
    Add a test for a sentence with duplicate words.

--- markov_test.go
+++ markov_test.go
@@ -4,16 +4,29 @@ import "testing"
 
 func TestMarkovImport(t *testing.T) {
 
-	t.Run("SimpleSentence", func(t *testing.T) {
+	t.Run("Simple Sentence", func(t *testing.T) {
 
-		c := NewChain()
+		c := NewChain(1)
 		s := "June jumps up and down."
 		c.Update(s)
 
 		t.Run("WordCount", func(t *testing.T) {
 
 			if c.Count() != 5 {
+				t.Fail()
+			}
+		})
+	})
 
+	t.Run("Duplicate Words", func(t *testing.T) {
+
+		c := NewChain(1)
+		s := "The dog is near the bed."
+		c.Update(s)
+
+		t.Run("WordCount", func(t *testing.T) {
+
+			if c.Count() != 5 {
 				t.Fail()
 			}
 		})
